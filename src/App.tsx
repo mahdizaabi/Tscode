3b14d37bf50e317104331259a20263d4165f89a5
chiheblahouli <mahdi.zaabi@gmail.com>
2022-05-09 20:18:11 +0200

    [fix] remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import AuthProvider from 'auth/AuthProvider';
-import PATHS from 'routes/path';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import ThemeProviderComponent from 'theme/ThemeProvider';
-import Routes from 'routes/routes';
+import Routes from 'routes/Routes';
+import { Suspense } from 'react';
+import Loading from 'components/common/loading/Loading';
 
 
 function App() {
 
-  return (
-   <Router>
-       <AuthProvider>
-
-            <ThemeProviderComponent>
-                <Routes></Routes>
-            </ThemeProviderComponent>
-
-       </AuthProvider>
-   </Router>
-  );
+    return (
+        <Suspense fallback={<Loading />} >
+            <Router>
+                <AuthProvider>
+                    <ThemeProviderComponent>
+                        <Routes></Routes>
+                    </ThemeProviderComponent>
+                </AuthProvider>
+            </Router>
+        </Suspense>
+    );
 }
 
 export default App;
