6184c96bb4a2ff28bdcbb08a80abae3e13be752a
chiheblahouli <mahdi.zaabi@gmail.com>
2022-05-09 15:50:50 +0200

    [feature] add layout component

---
+++ src/components/common/layout/Layout.tsx
@@ -0,0 +1,27 @@
+import { PropsWithChildren } from 'react'
+import { styled } from '@mui/material/styles'
+import Header from '../header/Header'
+
+
+
+const LayoutContainer = styled('div')({
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+})
+
+const Page = styled('div')({
+    height: '100%'
+})
+const Layout = (props: PropsWithChildren<{}>) => {
+    return (
+        <LayoutContainer>
+            <Header></Header>
+            <Page>
+                {props.children}
+            </Page>
+        </LayoutContainer>
+    )
+}
+
+export default Layout
