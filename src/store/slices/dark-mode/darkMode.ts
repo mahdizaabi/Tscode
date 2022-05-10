7014d02c75e72cfbea8f390ee030465199aa38cf
chiheblahouli <mahdi.zaabi@gmail.com>
2022-05-10 22:45:24 +0200

    [feature] add dark mode state slice

---
+++ src/store/slices/dark-mode/darkMode.ts
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = false;
+
+const darkModeSlice = createSlice({
+    initialState,
+    name: 'darkMode',
+    reducers: {
+        toggleDarkMode(state) {
+            return !state;
+        },
+        ActionX(state) {
+            return !state
+        }
+    }
+})
+
+
+export const { toggleDarkMode} = darkModeSlice.actions;
+// export const { ActionX } = darkModeSlice.actions;
+
+const darkModeReducer = darkModeSlice.reducer;
+
+export default darkModeReducer;
